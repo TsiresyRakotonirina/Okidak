9ecba84d6089942e59d76abb5b5238ff3c1a9f1c
Tsix <tsiresyrakotonirina02@gmail.com>
2024-09-06 10:57:03 +0300

    06/09 débute carousel
    débuter gestion carousel

--- src/main/resources/static/js/modalVideo.js
+++ src/main/resources/static/js/modalVideo.js
@@ -76,9 +76,9 @@ if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
 
             function updatePlayButtonText() {
                 if (player && player.getPlayerState() === YT.PlayerState.PLAYING) {
-                    playButton.text('Pause Video'); 
+                    playButton.text('Pause'); 
                 } else {
-                    playButton.text('Play Video'); 
+                    playButton.text('Play'); 
                 }
             }
         }
@@ -102,6 +102,9 @@ if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
             if (player.getPlayerState() === YT.PlayerState.PLAYING) {
                 setTimeout(updateTime, 1000);
             }
+
+
+
         }
 
 
@@ -149,6 +152,8 @@ if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
             
             if (campaign_type == "Publication") {
 
+                // type campaign = publication
+
                 if (typeof player !== 'undefined') {
                     player.destroy(); // Détruire le lecteur YouTube existant
                     player = undefined; // Réinitialiser la variable player
@@ -157,6 +162,7 @@ if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
 
                 var videoElement = document.createElement('video');
                 videoElement.setAttribute('controls', ''); // Ajoute les contrôles de lecture
+                videoElement.setAttribute('id', 'video'); 
         
                 // Définir la source de la vidéo
                 var sourceElement = document.createElement('source');
@@ -170,59 +176,93 @@ if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
                 var playerDiv = document.getElementById('player');
                 playerDiv.innerHTML = ''; // Nettoyer tout contenu précédent
                 playerDiv.appendChild(videoElement);
+
+
+                // CONTROLLE VIDEO 
+
+                // Récupérer l'élément vidéo et la barre de progression
+                var video = document.getElementById('video');
+                var progressBar = document.getElementById('progress-bar');
+
+                // Mettre à jour la barre de progression lorsque la vidéo se joue
+                video.addEventListener('timeupdate', function() {
+                    var value = (100 / video.duration) * video.currentTime;
+                    progressBar.value = value;
+                });
+
+
+                // Mettre à jour la vidéo lorsque l'utilisateur fait glisser la barre de progression
+                progressBar.addEventListener('input', function() {
+                    var time = (progressBar.value / 100) * video.duration;
+                    video.currentTime = time;
+                });
+
+                // Contrôles Play/Pause
+                var playPauseButton = document.getElementById('playButton');
+
+                // Ajoute un écouteur d'événements pour le bouton
+                playPauseButton.addEventListener('click', function() {
+                    if (video.paused) {
+                        video.play();
+                        playPauseButton.textContent = 'Pause';
+                    } else {
+                        video.pause();
+                        playPauseButton.textContent = 'Play';
+                    }
+                });
                 
             } else {
 
-            // if type de campaign = Youtube :
+                //  type de campaign = Youtube :
 
-                        var video_Id = extractVideoId(videoUrl);
+                var video_Id = extractVideoId(videoUrl);
 
-                        console.log(player);
-                        console.log("mandalo else");
-
-
-                        // if (!player) {
-                            player = new YT.Player('player', {
-                                height: '100%',
-                                width: '100%',  
-                                videoId: video_Id, 
-                                playerVars: {
-                                    'controls': 0, // Désactive la barre de contrôle (lecture, pause, etc.)
-                                    'disablekb': 1, // Désactive les raccourcis clavier
-                                    'modestbranding': 1, // Réduit l'image de marque YouTube
-                                    'rel': 0, // Ne montre pas les vidéos suggérées à la fin
-                                    'showinfo': 0, // Masque les informations sur la vidéo (titre, uploader)
-                                    'fs': 0, // Désactive le bouton plein écran
-                                    'iv_load_policy': 3, // Désactive les annotations
-                                    'autohide': 1, // Masque les commandes après la lecture de la vidéo
-                                    'playsinline': 1, // Joue la vidéo en ligne au lieu de plein écran sur mobile
-                                    'enablejsapi': 1 // Active l'API JavaScript
-                                },
-                                events: {
-                                    'onReady': onPlayerReady,
-                                    'onError': function(event) {
-                                        console.error("Erreur dans le lecteur:", event.data);
-                                    },
-                                    'onStateChange': onPlayerStateChange
-                                }
-                            });
-                        // } else {
-                        //     player.loadVideoById(video_Id); // Change la vidéo et commence la lecture // youtube makany amn youtube
-                        // }
+                console.log(player);
+                console.log("mandalo else");
 
 
-                        playerReadyDeferred.done(function() {
-                            if (player && typeof player.cueVideoById === 'function') {
-                                player.cueVideoById(video_Id);
-                                console.log("Vidéo chargée avec ID:", video_Id);
-                            } else {
-                                console.error('La méthode cueVideoById n\'est pas disponible.');
-                            }
-                        }).fail(function() {
-                            console.error("Le lecteur n'est pas prêt.");
-                        });
+                // if (!player) {
+                    player = new YT.Player('player', {
+                        height: '100%',
+                        width: '100%',  
+                        videoId: video_Id, 
+                        playerVars: {
+                            'controls': 0, // Désactive la barre de contrôle (lecture, pause, etc.)
+                            'disablekb': 1, // Désactive les raccourcis clavier
+                            'modestbranding': 1, // Réduit l'image de marque YouTube
+                            'rel': 0, // Ne montre pas les vidéos suggérées à la fin
+                            'showinfo': 0, // Masque les informations sur la vidéo (titre, uploader)
+                            'fs': 0, // Désactive le bouton plein écran
+                            'iv_load_policy': 3, // Désactive les annotations
+                            'autohide': 1, // Masque les commandes après la lecture de la vidéo
+                            'playsinline': 1, // Joue la vidéo en ligne au lieu de plein écran sur mobile
+                            'enablejsapi': 1 // Active l'API JavaScript
+                        },
+                        events: {
+                            'onReady': onPlayerReady,
+                            'onError': function(event) {
+                                console.error("Erreur dans le lecteur:", event.data);
+                            },
+                            'onStateChange': onPlayerStateChange
+                        }
+                    });
+                // } else {
+                //     player.loadVideoById(video_Id); // Change la vidéo et commence la lecture // youtube makany amn youtube
+                // }
 
+
+                playerReadyDeferred.done(function() {
+                    if (player && typeof player.cueVideoById === 'function') {
+                        player.cueVideoById(video_Id);
+                        console.log("Vidéo chargée avec ID:", video_Id);
+                    } else {
+                        console.error('La méthode cueVideoById n\'est pas disponible.');
                     }
+                }).fail(function() {
+                    console.error("Le lecteur n'est pas prêt.");
+                });
+
+            }
 
 
         });
@@ -237,9 +277,10 @@ if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
             }
 
             // Arrêter la vidéo HTML5 si elle existe
-            var videoElement = document.querySelector('#player');
+            // var videoElement = document.querySelector('#player');
+            var videoElement = document.getElementById('video');
             if (videoElement) {
-                // videoElement.pause();
+                videoElement.pause();
                 videoElement.src = '';
             }
             
